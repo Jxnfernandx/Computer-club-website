9b93fcf060b3877b0365b9034b130c2a1281950c
jxnfernandx <orangutangklan@gmail.com>
2024-02-08 19:43:23 -0600

    Added hamburger menu dropdown function

--- des/script.js
+++ des/script.js
@@ -1,6 +1,7 @@
 "use strict";
 const burgerIcon = document.getElementById("burger-icon");
 const hotdogIcon = document.getElementById("hotdog-icon");
+const links = document.getElementById("links");
 // Define a function to toggle the display of icons
 function toggleIcons(iconToShow, iconToHide) {
     iconToHide.style.display = "none";
@@ -9,7 +10,9 @@ function toggleIcons(iconToShow, iconToHide) {
 // Add click event listeners to both icons
 burgerIcon.addEventListener("click", (event) => {
     toggleIcons(hotdogIcon, burgerIcon);
+    links.style.display = "flex";
 });
 hotdogIcon.addEventListener("click", (event) => {
     toggleIcons(burgerIcon, hotdogIcon);
+    links.style.display = "none";
 });
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,7 @@
 const burgerIcon = document.getElementById("burger-icon") as HTMLElement;
 const hotdogIcon = document.getElementById("hotdog-icon") as HTMLElement;
+const links = document.getElementById("links") as HTMLElement;
+
 
 // Define a function to toggle the display of icons
 function toggleIcons(iconToShow: HTMLElement, iconToHide: HTMLElement) {
@@ -10,10 +12,12 @@ function toggleIcons(iconToShow: HTMLElement, iconToHide: HTMLElement) {
 // Add click event listeners to both icons
 burgerIcon.addEventListener("click", (event: MouseEvent) => {
     toggleIcons(hotdogIcon, burgerIcon);
+    links.style.display = "flex";
 });
 
 hotdogIcon.addEventListener("click", (event: MouseEvent) => {
     toggleIcons(burgerIcon, hotdogIcon);
+    links.style.display = "none";
 });
 
 
